3b46e3d3f761a46112e510720230ccc168724593
Yan-ta <yatashe@yandex.ru>
2022-05-22 18:48:02 +0300

    Initial Commit

---
+++ CoreDataApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataApp
+//
+//  Created by Ян Таше on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
